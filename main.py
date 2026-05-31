e555c8867322df9ff18335bc4263d267788f07f6
Dungewar <dungewar@gmail.com>
2026-05-31 15:52:18 -0700

    Made drone go in circle

--- main.py
+++ main.py
@@ -12,13 +12,16 @@ try:
 
     time.sleep(2)
 
-    tello.move_forward(50)
-
-    time.sleep(2)
+    total = 0
+    while total < 360:
+        tello.move_forward(5)
+        tello.rotate_clockwise(5)
+        total += 5
+    
 
     tello.land()
 except BaseException as e:
     print(f"{e}")
-    print("\x1b[31;1mCan't connect. Are you connected to the drone's network?\x1b[0m")
+    print("\x1b[31;1mCan't connect. Are you connected to the drone's network? Is your VPN on?\x1b[0m")
 
 
